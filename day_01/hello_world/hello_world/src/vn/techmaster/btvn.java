ae9262a9ad97856e6228befb354817a2331be97d
tieudiep17494 <kiritodiep17494@gmail.com>
2022-04-20 21:55:59 +0700

    Create btvn.java

---
+++ day_01/hello_world/hello_world/src/vn/techmaster/btvn.java
@@ -0,0 +1,41 @@
+package vn.techmaster;
+
+import java.util.Locale;
+
+public class btvn {
+
+
+
+        public static void main(String[] args) {
+
+            int a = 3, b = 2;
+
+            System.out.println(a+b);
+            System.out.println(a-b);
+            System.out.println(a*b);
+            System.out.println(a/b);
+            System.out.println(b-a);
+            System.out.println(b/a);
+
+
+
+
+            double chieuCao, canNang, chiSoBMI;
+
+
+            System.out.println(chieuCao = 1.7);
+            System.out.println(canNang = 50);
+            System.out.println(chiSoBMI = canNang / Math.pow(chieuCao,2));
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+
