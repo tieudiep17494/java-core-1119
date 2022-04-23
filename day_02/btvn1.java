14340edf2632f140393324a72a918448837ef140
tieudiep17494 <kiritodiep17494@gmail.com>
2022-04-23 16:13:09 +0700

    Create btvn1.java

---
+++ day_02/btvn1.java
@@ -0,0 +1,31 @@
+package vn.techmaster;
+
+
+import java.util.Scanner;
+
+public class btvn1 {
+    public static void main(String[] args) {
+
+        Scanner sc;
+        sc = new sc(System.in);
+
+        System.out.println("nhập vào chiều dài : ");
+        double length = sc.nextDouble();
+
+
+        System.out.println("nhập vào chiều rộng : ");
+        double width = sc.nextDouble();
+
+        double chuVi = (length + width) * 2;
+
+        double dienTich = length * width;
+
+        System.out.println("Chu vi : " + chuVi);
+
+        System.out.println("Dien tich : " + dienTich);
+
+    }
+
+
+
+}
