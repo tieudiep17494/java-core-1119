8909c52c55f6513d7ea59dfa080573f03ef53d68
tieudiep17494 <102859747+tieudiep17494@users.noreply.github.com>
2022-05-26 16:02:22 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,8 @@
+import controller.UserController;
+
+public class main {
+    public static void main(String[] args) {
+        UserController userController = new UserController();
+        userController.run();
+    }
+}
